689a353f61afec15ad4f584ab341ebe1035c1a92
liuweihong <1981weihong@163.com>
2025-04-06 17:01:09 +0800

    add resut

--- rust_study/rust_example/src/main.rs
+++ rust_study/rust_example/src/main.rs
@@ -1,3 +1,17 @@
+// 导入模块
+mod datatype;
+use datatype::primitive_types::*;
+use datatype::array_types::*;
+use datatype::string_types::string_examples;
+use datatype::tuple_types::tuple_examples;
+
 fn main() {
     println!("Hello, world!");
+    primitive_types();
+    demonstrate_usize();
+    demonstrate_isize();
+    check_size_types();
+    array_examples();
+    string_examples();
+    tuple_examples();
 }
